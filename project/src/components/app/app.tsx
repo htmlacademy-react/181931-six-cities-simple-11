6a30769e48642d71e964bd5156f54c89c7d492fa
alesukhin <alesukhin@museumplus.ru>
2022-10-21 18:10:46 +0300

    Добавляет страницы и компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,18 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import React from 'react';
+import MainScreen from '../../pages/main-screen/main-screen';
+import Header from '../header/header';
+
+type AppScreenProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppScreenProps): JSX.Element {
+  return (
+    <React.Fragment>
+      <Header />
+      <MainScreen offersCount={offersCount}/>
+    </React.Fragment>
+  );
 }
 
 export default App;
