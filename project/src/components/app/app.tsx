6efd091595dd7cfaa8bdf4b8ec975a4c234c096e
alesukhin <alesukhin@museumplus.ru>
2022-10-24 11:47:16 +0300

    Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,6 +1,11 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import React from 'react';
+import {AppRoute} from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
-import Header from '../header/header';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import PropertyScreen from '../../pages/property-screen/property-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import Layout from '../layout';
 
 type AppScreenProps = {
   offersCount: number;
@@ -8,10 +13,31 @@ type AppScreenProps = {
 
 function App({offersCount}: AppScreenProps): JSX.Element {
   return (
-    <React.Fragment>
-      <Header />
-      <MainScreen offersCount={offersCount}/>
-    </React.Fragment>
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<Layout />}>
+          <Route
+            index
+            element={<MainScreen offersCount={offersCount} />}
+          />
+          <Route
+            path={AppRoute.Login}
+            element={
+              <LoginScreen/>
+            }
+          />
+          <Route
+            path={AppRoute.Room}
+            element={<PropertyScreen />}
+          />
+          <Route
+            path="*"
+            element={<NotFoundScreen />}
+          />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
